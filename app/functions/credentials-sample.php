7abc0b5f9e163dee63aaf71e1bdca159a5570ba2
Vladimir Pezzole <vladimir.hipermidia@gmail.com>
2023-03-15 11:41:45 -0300

    incluir credentials-sample

---
+++ app/functions/credentials-sample.php
@@ -0,0 +1,12 @@
+<?php
+// Rename credentials-sample.php para credentials.php
+//
+// $emailHost = 'Seu HOST';
+// $emailUsername = 'Seu Username';
+// $emailPassword = 'Seu Password';
+// $myEmail = 'Seu Email';
+
+$emailHost = 'Seu HOST';
+$emailUsername = 'Seu Username';
+$emailPassword = 'Seu Password';
+$myEmail = 'Seu Email';
